225c7361a02a70d36285fddd209bb51991b2c613
Luminance <50186248+AustraliaIsMyCity@users.noreply.github.com>
2022-05-13 19:19:57 +1000

    made tome scaling apply in steps every 10 minutes

--- game/scripts/vscripts/items/item_tome_of_knowledge.lua
+++ game/scripts/vscripts/items/item_tome_of_knowledge.lua
@@ -3,8 +3,8 @@ item_tome_of_knowledge_lua = item_tome_of_knowledge_lua or class({})
 function item_tome_of_knowledge_lua:OnSpellStart()
 	local caster = self:GetCaster()
 
-	local minute = math.floor(GameRules:GetDOTATime(false, false) / 60)
-	local exp = self:GetSpecialValueFor("xp_bonus") + minute * self:GetSpecialValueFor("xp_per_minute")
+	local ten_minutes = math.floor(GameRules:GetDOTATime(false, false) / 600)
+	local exp = self:GetSpecialValueFor("xp_bonus") + ten_minutes * self:GetSpecialValueFor("xp_per_minute") * 10
 
 	caster:AddExperience(exp, DOTA_ModifyXP_TomeOfKnowledge, false, true)
 	self:SpendCharge()
