935d74ab7ec155f6b73c9ff02da370b8ae061ba6
Luminance <50186248+AustraliaIsMyCity@users.noreply.github.com>
2022-05-13 19:25:38 +1000

    fixed tome giving +300 from minute 10 instead of 20

--- game/scripts/vscripts/items/item_tome_of_knowledge.lua
+++ game/scripts/vscripts/items/item_tome_of_knowledge.lua
@@ -3,7 +3,7 @@ item_tome_of_knowledge_lua = item_tome_of_knowledge_lua or class({})
 function item_tome_of_knowledge_lua:OnSpellStart()
 	local caster = self:GetCaster()
 
-	local ten_minutes = math.floor(GameRules:GetDOTATime(false, false) / 600)
+	local ten_minutes = math.floor(GameRules:GetDOTATime(false, false) / 600) - 1
 	local exp = self:GetSpecialValueFor("xp_bonus") + ten_minutes * self:GetSpecialValueFor("xp_per_ten_minutes")
 
 	caster:AddExperience(exp, DOTA_ModifyXP_TomeOfKnowledge, false, true)
