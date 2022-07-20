aa19604973c57cd9bc6caa4467a1df42555cc7ff
Sheodar <kkabachok@gmail.com>
2022-07-20 18:53:16 +0300

    Closed #607

--- content/panorama/layout/custom_game/notifications_ui/notifications_ui.js
+++ content/panorama/layout/custom_game/notifications_ui/notifications_ui.js
@@ -1,15 +1,15 @@
 function WeakTeamNotification(data) {
 	const panel = $.CreatePanel("Panel", $("#Norifications_TOP"), "");
 	panel.BLoadLayoutSnippet("WeakTeamBonus");
-	panel.FindChild("WeakHeader").SetDialogVariable("mmr_diff", data.mmrDiff);
-	panel.FindChildTraverse("WeakBonusExpText").SetDialogVariable("exp_pct", data.xp_multiplier);
-	panel.FindChildTraverse("WeakBonusGoldText").SetDialogVariable("gold_pct", data.gold_multiplier);
+
+	panel.SetDialogVariable("mmr_diff", data.mmrDiff);
+	panel.SetDialogVariable("exp_pct", data.xp_multiplier.toFixed(1));
+	panel.SetDialogVariable("gold_pct", data.gold_multiplier.toFixed(1));
+
 	panel.SetHasClass("show", true);
-	const closeEvent = function (panel) {
-		panel.FindChildTraverse("WeakClose").SetPanelEvent("onactivate", () => {
-			panel.SetHasClass("show", false);
-		});
-	};
-	closeEvent(panel);
+
+	panel.FindChildTraverse("WeakClose").SetPanelEvent("onactivate", () => {
+		panel.SetHasClass("show", false);
+	});
 }
 GameEvents.SubscribeProtected("WeakTeamNotification", WeakTeamNotification);
