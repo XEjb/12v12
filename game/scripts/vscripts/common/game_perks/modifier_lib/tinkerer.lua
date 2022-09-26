c418ad2fe8685a55f004b97e3e722a861533acaf
Jonathan Ezard <63279022+Draze22@users.noreply.github.com>
2022-09-27 00:05:42 +1000

    Fixed a typo

--- game/scripts/vscripts/common/game_perks/modifier_lib/tinkerer.lua
+++ game/scripts/vscripts/common/game_perks/modifier_lib/tinkerer.lua
@@ -94,6 +94,6 @@ tinkerer_t0.v = 1.25
 tinkerer_t1 = class(tinkerer)
 tinkerer_t1.v = 1.50
 tinkerer_t2 = class(tinkerer)
-tinkerer_t2.v = 5.00
+tinkerer_t2.v = 2.00
 tinkerer_t3 = class(tinkerer)
 tinkerer_t3.v = 3.00
