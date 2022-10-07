b6a55b7b27f8fecf2cf2ee1ccc80c2434b6d3979
Draze22 <63279022+Draze22@users.noreply.github.com>
2022-10-07 21:53:33 +1100

    Int per level perks nerfs

--- game/scripts/vscripts/common/game_perks/modifier_lib/bonus_int.lua
+++ game/scripts/vscripts/common/game_perks/modifier_lib/bonus_int.lua
@@ -13,7 +13,7 @@ bonus_int_t1 = class(bonus_int)
 bonus_int_t2 = class(bonus_int)
 bonus_int_t3 = class(bonus_int)
 
-function bonus_int_t0:OnCreated() self.v = {0, 1, 1} end
-function bonus_int_t1:OnCreated() self.v = {0, 1, 2} end
-function bonus_int_t2:OnCreated() self.v = {0, 1, 4} end
-function bonus_int_t3:OnCreated() self.v = {0, 1, 8} end
+function bonus_int_t0:OnCreated() self.v = {0, 1, 0.6} end
+function bonus_int_t1:OnCreated() self.v = {0, 1, 1.2} end
+function bonus_int_t2:OnCreated() self.v = {0, 1, 2.4} end
+function bonus_int_t3:OnCreated() self.v = {0, 1, 4.8} end
